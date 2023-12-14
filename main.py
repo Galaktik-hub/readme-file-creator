c41f25ca652ce4aaa312bd45c112ca125f5c469e
Alexis <56489898+Galaktik-hub@users.noreply.github.com>
2023-12-14 21:38:16 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+##########################################
+#           IMPORTING SECTION            #
+##########################################
+import PySimpleGUI as sg
+import tkinter
+import os
+##########################################
+'''
+This program lets you create freely a README file for your GitHub profile or your projects.
+
+Throught a graphical interface, you can type in your text and the way you want to formate it.
+
+Once you're done creating your README file, you can type the 'Finished!' button and it will create the file in your current directory.
+
+In the footer of the README, there will always be a credit to this program, I do not restreint you to remove it, thought crediting someone for their work is always good!
+'''
+
+#TODO: Implement a graphical interface. Using tkinter ? Or rather PySimpleGUI ?
+#TODO: Translate text into markdown language
+#TODO: Find a way to show how it is interpreted in a windows or a sub-section of the window
+#TODO: Add credit at the end (in a footer)
+#TODO: Once all the text is done, write it in a file
+#TODO: Start the project lol
+
+def window():
+    """Create the initial window"""
+    pass
+
+
+if __name__ == "__main__":
+    pass
