80873579143215ffc8825d8673f01dd4d339fb5d
Galaktik-hub <dvatheking@gmail.com>
2023-12-14 22:43:46 +0100

    More text formats

--- README_test.md
+++ README_test.md
@@ -1,4 +1,5 @@
-#Hello, world!
-
-##Here are a little things about me
-This is just a test!
+<!--Thank you for using my program, it really means a lot to me!If you have any question, suggestion, or you just want to say hi, don't hesitate to check my GitHub page: https://github.com/Galaktik-hubDon't forget to leave the repository of this project a star!-->
+# This is just a test, I'm trying to 
+```
+const test = console.log("Hello, World!")
+```
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Throught a graphical interface, you can type in your text and the way you want t
 
 Once you're done creating your README file, you can type the 'Finished!' button and it will create the file in your current directory.
 
-In the footer of the README, there will always be a credit to this program, I do not restreint you to remove it, thought crediting someone for their work is always good!
+In the commentary of the README, there will always be a credit to this program, I do not restreint you to remove it, thought crediting someone for their work is always good!
 '''
 ##########################################
 #           IMPORTING SECTION            #
@@ -17,14 +17,15 @@ import os
 ##########################################
 #               GLOBAL VAR               #
 ##########################################
-TYPE_FORMAT = ['header1', 'header2', 'header3', 'normal', 'line_break']
-TYPE_FORMAT_FOR_FILE = ['#', '##', '###', '', '']
+TYPE_FORMAT = ['header1', 'header2', 'header3', 'normal', 'line_break', 'bold', 'italic', 'crossed', 'bolditalic', 'sub', 'sup', 'quote', 'code']
+TYPE_FORMAT_FOR_FILE_OPENING = ['# ', '## ', '### ', '', '', '**', '__', '~~', '***', '<sub>', '<sup>', '> ', '```\n']
+TYPE_FORMAT_FOR_FILE_CLOSING = ['', '', '', '', '', '**', '__', '~~', '***', '</sub>', '</sup>', '', '\n```']     # Some formats requires closing arguments, such as bold, italic, etc.
 ##########################################
 
 #TODO: Implement every functionnality that markdown offers (Maybe not the 3D model thought) https://docs.github.com/fr/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
 #TODO: Implement a graphical interface. Using tkinter ? Or rather PySimpleGUI ?
 #TODO: Find a way to show how it is interpreted in a window or a sub-section of the main window
-#TODO: Add credit at the end (in a footer)
+#TODO: Add credit at the beginning (In a commentary)
 #TODO: Once all the text is done, write it in a file
 
 
@@ -38,12 +39,20 @@ if __name__ == '__main__':
         ask_type = input('What kind of thing you want to write ? ')
         if ask_type == 'write':
             break
-        if ask_type == 'line_break':
+        elif ask_type == 'line_break':
             ask_texte = ''
         else:
             ask_texte = input('Insert your text: ')
         writing.append((TYPE_FORMAT.index(ask_type), ask_texte))
+
+    # This is the part where everything you wrote is going to be put into a README.md file
     with open('README_test.md', 'w') as file:
+        file.write('<!--')
+        file.write('Thank you for using my program, it really means a lot to me!')
+        file.write('If you have any question, suggestion, or you just want to say hi, don\'t hesitate to check my GitHub page: https://github.com/Galaktik-hub')
+        file.write('Don\'t forget to leave the repository of this project a star!')
+        file.write('-->')
+        file.write('\n')
         for line in writing:
-            file.write(TYPE_FORMAT_FOR_FILE[line[0]] + line[1] + '\n')
+            file.write(TYPE_FORMAT_FOR_FILE_OPENING[line[0]] + line[1] + TYPE_FORMAT_FOR_FILE_CLOSING[line[0]] + '\n')
         
